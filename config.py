8c5919103e1da5c2faf962f7e147113cf20b60eb
MikeyBeez <mbonsign@gmail.com>
2024-08-18 00:02:16 -0500

    changed model in config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Default configuration
 USER_NAME = "Mike Bee"
-MODEL_NAME = "llama3.1:latest"
+MODEL_NAME = "mistral:latest"
 
 # You can override these values here for a custom default configuration
 # USER_NAME = "CustomUser"
